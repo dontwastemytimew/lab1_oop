a736bf386bad69ddd8d627205cc40a844550f43f
dontwastemytimew <dontwastemytimew@users.noreply.github.com>
2026-02-23 16:16:50 +0000

    deploy: 8c4d19e5f1b1756ab04f22930bdcd11d6f5ea79b

--- search/all_1a.js
+++ search/all_1a.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['ключових_20термінів_20проєкту_20a_0',['* &lt;strong&gt;Глосарій:&lt;/strong&gt; &lt;a href=&quot;glossary.txt&quot; &gt;Опис ключових термінів проєкту&lt;/a&gt;.',['../index.html#autotoc_md3',1,'']]],
+  ['ключових_20термінів_20проєкту_3a_20a_20href_20https_3a_20github_20com_20dontwastemytimew_20lab1_5foop_20blob_20main_20glossary_20txt_20glossary_20txt_20a_0',['* &lt;strong&gt;Глосарій:&lt;/strong&gt; Опис ключових термінів проєкту: &lt;a href=&quot;https://github.com/dontwastemytimew/lab1_oop/blob/main/glossary.txt&quot;&gt;glossary.txt&lt;/a&gt;.',['../index.html#autotoc_md3',1,'']]],
   ['ключові_20впровадження_3a_1',['Ключові впровадження:',['../index.html#autotoc_md5',1,'']]]
 ];
--- search/all_8.js
+++ search/all_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['href_20glossary_20txt_20опис_20ключових_20термінів_20проєкту_20a_0',['* &lt;strong&gt;Глосарій:&lt;/strong&gt; &lt;a href=&quot;glossary.txt&quot; &gt;Опис ключових термінів проєкту&lt;/a&gt;.',['../index.html#autotoc_md3',1,'']]]
+  ['href_20https_3a_20github_20com_20dontwastemytimew_20lab1_5foop_20blob_20main_20glossary_20txt_20glossary_20txt_20a_0',['* &lt;strong&gt;Глосарій:&lt;/strong&gt; Опис ключових термінів проєкту: &lt;a href=&quot;https://github.com/dontwastemytimew/lab1_oop/blob/main/glossary.txt&quot;&gt;glossary.txt&lt;/a&gt;.',['../index.html#autotoc_md3',1,'']]],
+  ['https_3a_20github_20com_20dontwastemytimew_20lab1_5foop_20blob_20main_20glossary_20txt_20glossary_20txt_20a_1',['* &lt;strong&gt;Глосарій:&lt;/strong&gt; Опис ключових термінів проєкту: &lt;a href=&quot;https://github.com/dontwastemytimew/lab1_oop/blob/main/glossary.txt&quot;&gt;glossary.txt&lt;/a&gt;.',['../index.html#autotoc_md3',1,'']]]
 ];
