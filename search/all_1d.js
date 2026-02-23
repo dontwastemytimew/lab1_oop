5a5986f496520dc8662298c69817cc769da3e4ea
dontwastemytimew <dontwastemytimew@users.noreply.github.com>
2026-02-23 16:19:28 +0000

    deploy: d28bc39026250c42a1d91395e14f4e1bec4ed7ee

--- search/all_1d.js
+++ search/all_1d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['продуктивності_20benchmarking_0',['Аналіз продуктивності (Benchmarking)',['../index.html#autotoc_md9',1,'']]],
-  ['проєкту_3a_20a_20href_20https_3a_20github_20com_20dontwastemytimew_20lab1_5foop_20blob_20main_20glossary_20txt_20glossary_20txt_20a_1',['* &lt;strong&gt;Глосарій:&lt;/strong&gt; Опис ключових термінів проєкту: &lt;a href=&quot;https://github.com/dontwastemytimew/lab1_oop/blob/main/glossary.txt&quot;&gt;glossary.txt&lt;/a&gt;.',['../index.html#autotoc_md3',1,'']]],
+  ['проєкту_3a_20n_20link_20https_3a_20github_20com_20dontwastemytimew_20lab1_5foop_20blob_20main_20glossary_20txt_20glossary_20txt_20endlink_1',['* &lt;strong&gt;Глосарій:&lt;/strong&gt; Опис ключових термінів проєкту: \n @link https://github.com/dontwastemytimew/lab1_oop/blob/main/glossary.txt glossary.txt @endlink',['../index.html#autotoc_md3',1,'']]],
   ['про_20виконання_20reports_2',['Звіти про виконання (Reports)',['../index.html#autotoc_md2',1,'']]]
 ];
--- search/all_5.js
+++ search/all_5.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['exportservice_0',['ExportService',['../classExportService.html',1,'']]],
-  ['exporttojson_1',['exportToJson',['../classExportService.html#afe19373ee2997cef00ef30d1bc507e0c',1,'ExportService']]],
-  ['exporttopdf_2',['exportToPdf',['../classExportService.html#a0cb9f07757221a2990f5f9b7f2f1d7ee',1,'ExportService']]]
+  ['endlink_0',['* &lt;strong&gt;Глосарій:&lt;/strong&gt; Опис ключових термінів проєкту: \n @link https://github.com/dontwastemytimew/lab1_oop/blob/main/glossary.txt glossary.txt @endlink',['../index.html#autotoc_md3',1,'']]],
+  ['exportservice_1',['ExportService',['../classExportService.html',1,'']]],
+  ['exporttojson_2',['exportToJson',['../classExportService.html#afe19373ee2997cef00ef30d1bc507e0c',1,'ExportService']]],
+  ['exporttopdf_3',['exportToPdf',['../classExportService.html#a0cb9f07757221a2990f5f9b7f2f1d7ee',1,'ExportService']]]
 ];
