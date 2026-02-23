d0fb9e4000a083e464a0b713ea7aa5ba43360f7d
dontwastemytimew <dontwastemytimew@users.noreply.github.com>
2026-02-23 15:56:04 +0000

    deploy: b00fe208ef88a79b03a44a51db3be1f8c7d863ce

--- search/all_4.js
+++ search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['design_20notes_20manager_0',['Lab 1: Refactoring &amp; Object-Oriented Design (Notes Manager)',['../index.html',1,'']]],
-  ['dontwastemytimew_20lab1_5foop_20blob_20main_20glossary_20txt_20strong_20glossary_20txt_20strong_20a_1',['* &lt;strong&gt;Глосарій:&lt;/strong&gt; Опис ключових термінів проєкту: &lt;a href=&quot;https://github.com/dontwastemytimew/lab1_oop/blob/main/glossary.txt&quot; &gt;&lt;strong&gt;glossary.txt&lt;/strong&gt;&lt;/a&gt;.',['../index.html#autotoc_md3',1,'']]]
+  ['dontwastemytimew_20lab1_5foop_20blob_20main_20glossary_20txt_20glossary_20txt_20a_1',['* &lt;strong&gt;Глосарій:&lt;/strong&gt; Опис ключових термінів проєкту: &lt;a href=&quot;https://github.com/dontwastemytimew/lab1_oop/blob/main/glossary.txt&quot; &gt;glossary.txt&lt;/a&gt;.',['../index.html#autotoc_md3',1,'']]]
 ];
